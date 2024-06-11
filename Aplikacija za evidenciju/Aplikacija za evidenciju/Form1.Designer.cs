726d05f81465dc46f7d5520ba666b52fa51c8be1
Noah23Mautner <152974783+Noah23Mautner@users.noreply.github.com>
2024-06-11 13:07:01 +0200

    Add files via upload

---
+++ Aplikacija za evidenciju/Aplikacija za evidenciju/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Aplikacija_za_evidenciju
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(50, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Dodaj osobu";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(50, 121);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 26);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Prikazi Osobe";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(50, 164);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(85, 28);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Obradi";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(50, 211);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(85, 28);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Sortiraj";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(50, 260);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(85, 28);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Obrisi";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(50, 306);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(85, 27);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Spremi";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(50, 353);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(85, 24);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Ucitaj";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(254, 72);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(254, 121);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 9;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(404, 72);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(384, 167);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.Text = "";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(255, 174);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 12;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(255, 224);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 28);
+            this.comboBox2.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ Aplikacija za evidenciju/Aplikacija za evidenciju/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace Aplikacija_za_evidenciju
+{
+    public partial class Form1 : Form
+    {
+        private List<Osoba> listaOsoba = new List<Osoba>();
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add("Učenik");
+            comboBox1.Items.Add("Profesor");
+            comboBox1.Items.Add("Tehničko osoblje");
+            comboBox2.Items.Add("Učenik");
+            comboBox2.Items.Add("Profesor");
+            comboBox2.Items.Add("Tehničko osoblje");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Osoba osoba = new Osoba
+            {
+                Ime = textBox1.Text,
+                Prezime = textBox2.Text,
+                Uloga = comboBox1.SelectedItem.ToString()
+            };
+            listaOsoba.Add(osoba);
+            textBox1.Clear();
+            textBox2.Clear();
+            comboBox1.SelectedIndex = -1;
+            comboBox2.SelectedIndex = -1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            foreach (Osoba osoba in listaOsoba)
+            {
+                richTextBox1.AppendText(osoba.Ime + "\t" + osoba.Prezime + "\t" + osoba.Uloga + "\t" + osoba.Aktivnost + "\n");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            foreach (Osoba osoba in listaOsoba)
+            {
+                switch (osoba.Uloga)
+                {
+                    case "Učenik":
+                        osoba.Aktivnost = "Uči";
+                        break;
+                    case "Profesor":
+                        osoba.Aktivnost = "Predaje";
+                        break;
+                    case "Tehničko osoblje":
+                        osoba.Aktivnost = "Održava";
+                        break;
+                }
+            }
+            richTextBox1.Clear();
+            foreach (Osoba osoba in listaOsoba)
+            {
+                richTextBox1.AppendText(osoba.Ime + "\t" + osoba.Prezime + "\t" + osoba.Uloga + "\t" + osoba.Aktivnost + "\n");
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listaOsoba.Sort((x, y) => x.Ime.CompareTo(y.Ime));
+            richTextBox1.Clear();
+            foreach (Osoba osoba in listaOsoba)
+            {
+                richTextBox1.AppendText(osoba.Ime + "\t" + osoba.Prezime + "\t" + osoba.Uloga + "\t" + osoba.Aktivnost + "\n");
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text == "")
+            {
+                MessageBox.Show("Nema selektirane osobe!", "Greška", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string[] selectedOsobaDetails = richTextBox1.Text.Split('\t');
+            Osoba selektiranaOsoba = listaOsoba.FirstOrDefault(o => o.Ime == selectedOsobaDetails[0] && o.Prezime == selectedOsobaDetails[1]);
+
+            if (selektiranaOsoba == null)
+            {
+                MessageBox.Show("Osoba nije pronađena!", "Greška", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (MessageBox.Show("Da li ste sigurni da želite obrisati osobu?", "Brisanje osobe", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                listaOsoba.Remove(selektiranaOsoba);
+                richTextBox1.Clear();
+                foreach (Osoba osoba in listaOsoba)
+                {
+                    richTextBox1.AppendText(osoba.Ime + "\t" + osoba.Prezime + "\t" + osoba.Uloga + "\t" + osoba.Aktivnost + "\n");
+                }
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter writer = new StreamWriter("osobe.csv"))
+            {
+                foreach (Osoba osoba in listaOsoba)
+                {
+                    writer.WriteLine(osoba.Ime + "," + osoba.Prezime + "," + osoba.Uloga + "," + osoba.Aktivnost);
+                }
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            listaOsoba.Clear();
+            using (StreamReader reader = new StreamReader("osobe.csv"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] parts = line.Split(',');
+                    Osoba osoba = new Osoba
+                    {
+                        Ime = parts[0],
+                        Prezime = parts[1],
+                        Uloga = parts[2],
+                        Aktivnost = parts[3]
+                    };
+                    listaOsoba.Add(osoba);
+                }
+            }
+            richTextBox1.Clear();
+            foreach (Osoba osoba in listaOsoba)
+            {
+                richTextBox1.AppendText(osoba.Ime + "\t" + osoba.Prezime + "\t" + osoba.Uloga + "\t" + osoba.Aktivnost + "\n");
+            }
+        }
+        public class Osoba
+        {
+            public string Ime { get; set; }
+            public string Prezime { get; set; }
+            public string Uloga { get; set; }
+            public string Aktivnost { get; set; }
+        }
+    }
+}
